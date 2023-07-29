5272cca8436c5fc851fe9319ed798f987011b8df
JuliErre <86995775+JuliErre@users.noreply.github.com>
2023-07-29 14:09:11 -0300

    new api url

--- src/data/Api.js
+++ src/data/Api.js
@@ -1,5 +1,5 @@
 const Api = {
-    baseUrl: 'https://moviesapi.up.railway.app/api',
+    baseUrl: 'https://movies-api-ypg9.onrender.com/api',
     // baseUrl: 'http://localhost:3004/api',
     baseUrlImage:'https://image.tmdb.org/t/p/original'
 }
